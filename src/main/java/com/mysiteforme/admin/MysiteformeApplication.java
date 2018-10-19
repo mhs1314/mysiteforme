079f6621d4ccc6e0acd08997fdc75204e1f4ff5c
471002385@qq.com <471002385@qq.com>
2018-10-19 13:31:49 +0800

    部署

--- src/main/java/com/mysiteforme/admin/MysiteformeApplication.java
+++ src/main/java/com/mysiteforme/admin/MysiteformeApplication.java
@@ -3,11 +3,10 @@ package com.mysiteforme.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.mysiteforme.admin.dao")
-public class MysiteformeApplication extends SpringBootServletInitializer {
+public class MysiteformeApplication{
     public static void main(String[] args) {
         SpringApplication.run(MysiteformeApplication.class, args);
     }
